7bec55ced63fd49e0bf1452e55980c660c13ce1c
Firozmahamud <firozmahmud593@gmail.com>
2025-12-06 17:02:38 +0600

    detect the file extension from selected folder

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
 
 	//it'll listen to user input from terminal
 	scanner := bufio.NewScanner(os.Stdin)
- 	fmt.Print("Please enter your folder Path: ")
+	fmt.Print("Enter your folder Path: ")
 	scanner.Scan()
 	path := scanner.Text()
 	// fmt.Println("your folder Path is :", path)
@@ -20,31 +22,19 @@ func main() {
 	count := len(files)
 
 	if err != nil {
-	fmt.Println("\n--------------- 404 path not found ---------------")
-		fmt.Println("\t Error reading directory path \n", err, "\n")
+		fmt.Println("\n--------------- 404 invalid path ---------------")
+		fmt.Println("\t \tEnter a valid path \n", err, "\n")
 		return
 	}
-	fmt.Println("your folder Path is :", path)
-	fmt.Println("Type of files  :", fmt.Sprintf("%T", files))
-	// fmt.Println("Type of err    :", fmt.Sprintf("%T", err))
-	// fmt.Println("Error value    :", err)
-	fmt.Println("Total entries of files  :", count)
-
-	
 
 	if count == 0 {
 		fmt.Println("This folder is empty.")
 		return
-	
+
 	}
-	fmt.Println("\n--------------- Number of Folder Content ---------------")
-	// for _, f := range files {
-	// 	if f.IsDir() {
-	// 		fmt.Println("[Folder] :", f.Name())
-	// 	} else {
-	// 		fmt.Println("[File]   :", f.Name())
-	// 	}
-	// }
+	fmt.Println("\n--------------- Total Number of Folder Content ---------------")
+	fmt.Println("            ---------------   ", count, "     ---------------      \n")
+
 	//aded counter for files and folders
 	fileCount := 0
 	folderCount := 0
@@ -53,15 +43,22 @@ func main() {
 		if f.IsDir() {
 			folderCount++
 			index := fmt.Sprintf("%02d", folderCount)
-			fmt.Printf("[Folder] %s : %s\n", index, f.Name())
+			fmt.Printf("[Folder]      %s : %s\n", index, f.Name())
 		} else {
 			fileCount++
 			index := fmt.Sprintf("%02d", fileCount)
-			fmt.Printf("[File]   %s : %s\n", index, f.Name())
+			fmt.Printf("[File]        %s : %s\n", index, f.Name())
 		}
-}
+
+		//detect the File Extension
+		ext := strings.ToLower(filepath.Ext(f.Name()))
+		fmt.Println("[File Extension] :", ext,"\n")
+
+	}
+
+	
 }
 
-func init(){
+func init() {
 	fmt.Println("----------- WelCome to Folder Organizer -------------")
 }
